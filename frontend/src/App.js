82680c5721021bd14ec9b28b8e9631ee3f203ea4
Amriteshsingh94 <amriteshsingh2000n@gmail.com>
2024-01-28 02:52:43 +0530

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+import "./App.css";
+import Header from "./component/layout/Header.js"
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      helloooooo
-    </div>
+    <Router>
+      <Header />
+    </Router>
   );
 }
 
