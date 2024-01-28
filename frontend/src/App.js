ba99ad51d87f1f3494bb6056c69c5c587dd30c0a
Amriteshsingh94 <amriteshsingh2000n@gmail.com>
2024-01-28 18:42:22 +0530

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
 import Header from "./component/layout/Header/Header.js"
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import webfont from "webfontloader";
 import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/layout/Home/Home.js";
 
 
 function App() {
@@ -19,7 +20,9 @@ function App() {
   return (
     <Router>
       <Header />
-
+      <Routes>
+        <Route extact path="/" Component={Home} />
+      </Routes>
 
       <Footer />
     </Router>
--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -7,19 +7,19 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer id="footer">
-            <div class='leftfooter'>
+            <div className='leftfooter'>
                 <h4>DOWNLOAD OUR APP</h4>
                 <p>Download App for Android and IOS mobile phone</p>
                 <img src={playStore} alt='PlayStore' />
                 <img src={appStore} alt='AppStore' />
 
             </div>
-            <div class='midfooter'>
+            <div className='midfooter'>
                 <h1>ElectroXpress</h1>
                 <p>Delivering high quality at minimum cost, where value meets affordability.</p>
                 <p>Copyrights 2023 &copy; AmriteshSingh </p>
             </div>
-            <div class='rightfooter'>
+            <div className='rightfooter'>
                 <h4>Follow Us</h4>
                 <a href='/#'>Instragram</a>
                 <a href='/#'>Facebook</a>
