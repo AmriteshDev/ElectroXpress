e1dec971a49c42d43768ffea9a5766ad38909850
Amriteshsingh94 <amriteshsingh2000n@gmail.com>
2024-01-28 21:45:34 +0530

    wip

--- frontend/src/component/layout/Home/Home.js
+++ frontend/src/component/layout/Home/Home.js
@@ -1,8 +1,14 @@
 import React, { Fragment } from 'react';
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
+import ProductCard from './Product/ProductCard.js';
 
-import Product from "./Product/Product.js";
+const product = {
+    name: "Blue Tshirt",
+    image: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+    _id: "amritesh",
+    price: "2000"
+}
 
 const Home = () => {
     return (
@@ -22,7 +28,16 @@ const Home = () => {
             <h2 className='homeHeading'>Featured Prducts</h2>
 
             <div className='container' id="container">
-                <Product />
+                <ProductCard product={product} />
+                <ProductCard product={product} />
+                <ProductCard product={product} />
+                <ProductCard product={product} />
+
+                <ProductCard product={product} />
+                <ProductCard product={product} />
+                <ProductCard product={product} />
+                <ProductCard product={product} />
+
             </div>
         </Fragment>
     )
