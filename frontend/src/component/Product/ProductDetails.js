58b0c11c0b32688627c53214b274d1245c1b7687
Amriteshsingh94 <amriteshsingh2000n@gmail.com>
2024-02-06 11:48:25 +0530

    wip

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -25,6 +25,7 @@ function ProductDetails() {
     }
 
     useEffect(() => {
+
         if (error) {
             alert.error(error);
             dispatch(clearErrors());
@@ -61,7 +62,7 @@ function ProductDetails() {
                                     <div className='detailsBlock-3-1'>
                                         <div className='detailsBlock-3-1-1'>
                                             <button>-</button>
-                                            <input type="number" value="1" />
+                                            <input type="number" defaultValue="1" />
                                             <button>+</button>
                                         </div>
                                         <button>Add to Cart</button>
@@ -89,7 +90,7 @@ function ProductDetails() {
                             product.reviews && product.reviews[0] ?
                                 (<div className='reviews'>
                                     {
-                                        product.reviews && product.reviews.map((review) => <ReviewCard review={review} />)
+                                        product.reviews && product.reviews.map((review) => <ReviewCard review={review} key={review.user} />)
                                     }
                                 </div>) : (<p className='noReviews'>No Reviews Yet</p>)
                         }
